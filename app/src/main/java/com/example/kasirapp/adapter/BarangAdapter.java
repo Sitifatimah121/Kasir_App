e05ffd365b6a59c83f6fd8f9c124c078a1ee1c1a
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-16 18:33:11 +0700

    implement ui in adapater

--- app/src/main/java/com/example/kasirapp/adapter/BarangAdapter.java
+++ app/src/main/java/com/example/kasirapp/adapter/BarangAdapter.java
@@ -1,11 +1,15 @@
 package com.example.kasirapp.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.kasirapp.R;
 import com.example.kasirapp.entity.Barang;
 
 import java.util.ArrayList;
@@ -34,22 +38,35 @@ public class BarangAdapter extends RecyclerView.Adapter<BarangAdapter.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.tvKode.setText(listBarang.get(position).getKode_barang());
+        holder.tvName.setText(listBarang.get(position).getNama_barang());
+        holder.tvJumlah.setText(listBarang.get(position).getJumlah_barang());
+        holder.tvDate.setText(listBarang.get(position).getDate());
+        holder.cvBarang.setOnClickListener(v -> onItemClickCallback.onItemClicked(listBarang.get(position), position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listBarang.size();
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView tvKode, tvName, tvJumlah, tvDate;
+        final CardView cvBarang;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvKode = itemView.findViewById(R.id.tv_item_code);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvJumlah = itemView.findViewById(R.id.tv_item_stok);
+            tvDate = itemView.findViewById(R.id.tv_item_date);
+            cvBarang = itemView.findViewById(R.id.cv_item_note);
         }
     }
 
