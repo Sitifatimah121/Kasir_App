74cd60b75bf6b9461e035979173ac0858681410b
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-16 13:47:53 +0700

    add bd contract

--- app/src/main/java/com/example/kasirapp/db/DatabaseContract.java
+++ app/src/main/java/com/example/kasirapp/db/DatabaseContract.java
@@ -1,5 +1,15 @@
 package com.example.kasirapp.db;
 
+import android.provider.BaseColumns;
+
 public class DatabaseContract {
 
+    static String TABLE_NAME = "barang";
+
+    static final class BarangColumns implements BaseColumns{
+        static  String KODE = "kode";
+        static String NAMA_BARANG = "nama_barang";
+        static int JUMLAH = 0;
+        static String DATE = "date";
+    }
 }
