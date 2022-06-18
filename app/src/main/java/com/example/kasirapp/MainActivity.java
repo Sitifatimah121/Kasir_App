e094e185f84fb33092a5bca51ffcfc3b6eecad06
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-18 09:25:32 +0700

    implement layout

--- app/src/main/java/com/example/kasirapp/MainActivity.java
+++ app/src/main/java/com/example/kasirapp/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.kasirapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private RecyclerView rvBarang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setTitle("Barang");
+
+        progressBar = findViewById(R.id.progressbar);
+        rvBarang = findViewById(R.id.rv_barang);
+        rvBarang.setLayoutManager(new LinearLayoutManager(this));
+        rvBarang.setHasFixedSize(true);
     }
 }
