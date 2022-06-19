8f7cc8db0bbde080988f480d111534b51408c495
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-19 22:20:52 +0700

    add intent barang

--- app/src/main/java/com/example/kasirapp/StartActivity.java
+++ app/src/main/java/com/example/kasirapp/StartActivity.java
@@ -2,13 +2,30 @@ package com.example.kasirapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button btnBarang = findViewById(R.id.btnBarang);
+        btnBarang.setOnClickListener(this);
+//        Button btnTrans = findViewById(R.id.btnTrans);
     }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnBarang){
+            Intent barangIntent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(barangIntent);
+        }
+    }
+
 }
